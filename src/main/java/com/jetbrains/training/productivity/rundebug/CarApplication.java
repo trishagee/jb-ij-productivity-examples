34c0784d14b9167fb17f6ab2b10230ed86bedda3
Trisha Gee <trisha.gee@gmail.com>
2025-11-23 07:54:23 +0100

    And I DID know that sysout was way easier in modern Java

--- src/main/java/com/jetbrains/training/productivity/rundebug/CarApplication.java
+++ src/main/java/com/jetbrains/training/productivity/rundebug/CarApplication.java
@@ -2,9 +2,11 @@ package com.jetbrains.training.productivity.rundebug;
 
 import com.jetbrains.training.productivity.rundebug.car.Car;
 
+import static java.lang.IO.println;
+
 public class CarApplication     {
     static void main() {
         Car car = new Car("Ford", "Mondeo", Color.BLACK);
-        System.out.println(car);
+        println(car);
     }
 }
