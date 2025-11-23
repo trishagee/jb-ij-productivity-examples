b58becc1439e182acdab44c84fd6d262d507742b
Trisha Gee <trisha.gee@gmail.com>
2025-11-23 07:53:41 +0100

    Apparently Java 25 has cleaner syntax for applications. Thank you IntelliJ IDEA for telling me

--- src/main/java/com/jetbrains/training/productivity/rundebug/CarApplication.java
+++ src/main/java/com/jetbrains/training/productivity/rundebug/CarApplication.java
@@ -3,7 +3,7 @@ package com.jetbrains.training.productivity.rundebug;
 import com.jetbrains.training.productivity.rundebug.car.Car;
 
 public class CarApplication     {
-    public static void main(String[] args) {
+    static void main() {
         Car car = new Car("Ford", "Mondeo", Color.BLACK);
         System.out.println(car);
     }
